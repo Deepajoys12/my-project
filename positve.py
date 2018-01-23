410f40cb6ee28fc3ae20d8176c3f1ed1b450c7e5
Deepajoys <joyciimmaculate@gmailcom>
2018-01-23 14:35:28 +0530

    first commit

---
+++ positve.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      Deepa
+#
+# Created:     19/01/2018
+# Copyright:   (c) Deepa 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+ num = float(input("Enter a number: "))
+ if num > 0:
+   print("Positive number")
+ elif num == 0:
+   print("Zero")
+ else:
+   print("Negative number")
+if __name__ == '__main__':
+    main()
